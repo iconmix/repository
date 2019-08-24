663318b60b66e0dcc645ff32a76b30ad9fe2eedd
iconmix <telexxitfr@yahoo.fr>
2019-08-24 09:11:44 +0200

    mise a jour

---
+++ updaterepo.bat
@@ -0,0 +1,5 @@
+python update_repo.py ../skins-addons/resource.uisounds.iconmix ../skins-addons/script.iconmixtools ../skins-addons/skin.iconmix-jarvis ../skins-addons/skin.iconmix-krypton ../skins-addons/skin.iconmix-leia
+git add .
+git commit -a -m "mise a jour"
+git push > lastgit.txt
+
