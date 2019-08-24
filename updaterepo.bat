ebc1e2645617e95b25ce4e7bf1b6193b80aae1d1
iconmix <telexxitfr@yahoo.fr>
2019-08-24 16:25:56 +0200

    mise a jour

--- updaterepo.bat
+++ updaterepo.bat
@@ -1,4 +1,4 @@
-python update_repo.py ../skins-addons/resource.uisounds.iconmix ../skins-addons/script.iconmixtools ../skins-addons/skin.iconmix-jarvis ../skins-addons/skin.iconmix-krypton ../skins-addons/skin.iconmix-leia
+python update_repo.py ..\skins-addons\resource.uisounds.iconmix ..\skins-addons\script.iconmixtools ..\skins-addons\skin.iconmix-jarvis ..\skins-addons\skin.iconmix-krypton ..\skins-addons\skin.iconmix-leia
 git add .
 git commit -a -m "mise a jour"
 git push > lastgit.txt
