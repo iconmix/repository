a913bbb7ebc2a22f7e110e00e702e160974d1bfd
metropolicon <telexxingou@hotmail.com>
2019-08-24 16:48:17 +0200

    mise a jour 24/08/2019

--- update_repo.bat
+++ update_repo.bat
@@ -1,6 +1,7 @@
-python.exe update_repo.py d:\IconMix\skins-addons\resource.uisounds.iconmix d:\IconMix\skins-addons\script.iconmixtools d:\IconMix\skins-addons\skin.iconmix-jarvis d:\IconMix\skins-addons\skin.iconmix-krypton d:\IconMix\skins-addons\skin.iconmix-leia
+python.exe update_repo.py d:\IconMix\skins-addons\script.iconmixtools
 git add .
 git commit -a -m "mise a jour %date%"
 git push > lastgit.txt
 
 pause
+d:\IconMix\skins-addons\resource.uisounds.iconmix  d:\IconMix\skins-addons\skin.iconmix-jarvis d:\IconMix\skins-addons\skin.iconmix-krypton d:\IconMix\skins-addons\skin.iconmix-leia
