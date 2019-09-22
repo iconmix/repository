eb3e478f01085a37133add39c42d57b0b2658d27
metropolicon <telexxingou@hotmail.com>
2019-09-22 09:02:35 +0200

    update repo

--- update_repo.bat
+++ update_repo.bat
@@ -1,9 +1,6 @@
-
-
-
 python update_repo.py d:\iconmix\skins-addons\script.iconmixtools d:\iconmix\skins-addons\skin.iconmix-krypton d:\iconmix\skins-addons\skin.iconmix-leia d:\iconmix\skins-addons\resource.uisounds.iconmix
 rem python pdate-directory-structure.py
-pause
+
 git add .
 git commit -a -m "update repo"
 git push
