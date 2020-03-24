005187ec7036308b769c48098152ac25d07ba76e
metropolicon <telexxingou@hotmail.com>
2020-03-24 18:10:53 +0100

    update repo

--- update_repo.bat
+++ update_repo.bat
@@ -1,7 +1,7 @@
 python update_repo.py ..\skins-addons\script.iconmixtools ..\skins-addons\skin.iconmix-krypton ..\skins-addons\resource.uisounds.iconmix
 rem ..\skins-addons\skin.iconmix-leia 
 rem python pdate-directory-structure.py
-pause
+
 git add .
 git commit -a -m "update repo"
 git push
