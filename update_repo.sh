9953ca8ded31a1d3c7bde1834c2501bae71a3e67
root <root@serveur.home>
2021-03-24 22:01:00 +0100

    mise a jour

--- update_repo.sh
+++ update_repo.sh
@@ -1,6 +1,5 @@
-python2 update_repo.py ../skins-addons/script.iconmixtoolsmatrix ../skins-addons/skin.iconmix-matrix
-
-#../skins-addons/script.iconmixtools ../skins-addons/resource.uisounds.iconmix  ../skins-addons/skin.iconmix-krypton ../skins-addons/skin.iconmix-leia 
+python2 update_repo.py ../skins-addons/script.iconmixtoolsmatrix ../skins-addons/skin.iconmix-matrix ../skins-addons/script.iconmixtools 
+#../skins-addons/resource.uisounds.iconmix  ../skins-addons/skin.iconmix-krypton ../skins-addons/skin.iconmix-leia 
 
 git add .
 git commit -a -m "mise a jour"
